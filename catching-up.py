0d522a083d64c71045c34fa1d1e74e5e48f9f983
davidlsoto <davidls@uchicago.edu>
2021-04-02 10:25:18 -0500

    catching up with python

---
+++ catching-up.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr  2 10:22:27 2021
+# Title: Catching-up with Python 
+@author: DAVID
+"""
+# This code file contains old stuff learned last year. 
+
+print("Hello David")
+
+
